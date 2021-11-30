33e12e7baf8e83860b46f6ef4499d9089a3ce153
Anne Dubearnes <dubearna@tcd.ie>
2021-11-30 16:07:15 +0100

    update tidyverse exo

--- exercices/exo_tidyverse.R
+++ exercices/exo_tidyverse.R
@@ -28,15 +28,22 @@ ursidae<-species %>%
 ursidae<-ursidae %>% dplyr::filter(!stringr::str_detect(species, "malayanus"))
 
 #Join species table & sp_eco table
-sp_eco_ur<-dplyr::inner_join(ursidae,sp_eco)
+#sp_eco_ur<-dplyr::inner_join(ursidae,sp_eco)
+sp_eco_ur <- ursidae %>%
+  left_join(sp_eco)
 
 #Join sp_eco_ur & ecoreg
-ecoreg_ur<-dplyr::inner_join(sp_eco_ur,ecoreg)
+#ecoreg_ur<-dplyr::inner_join(sp_eco_ur,ecoreg)
+ecoreg_ur <- sp_eco_ur %>%
+  left_join(ecoreg, by = "ecoregion_id")
 
-#Group by species
-ecoreg_ur %>%
-  dplyr::group_by(species) %>%
+#Group by species and count number of biomes
+#ecoreg_ur %>%
+#  dplyr::group_by(species) %>%
+#  dplyr::across(species, dplyr::count(ecoregion))
 
-#Count number of biomes
-ecoreg_ur %>%
-  dplyr::count(species,biome)
+#count realms
+realm_ursus <- ecoreg_ur %>%
+  group_by(sci_name) %>%
+  summarise(n_realms     = n_distinct(realm))
+
